61a52fb0593ba856eb670765f885abf152013ffa
JTNghia <19521899@gm.uit.edu.vn>
2021-04-29 15:24:05 +0700

    update solverGA

--- File/SolvingKnapsackByGA/SolveKnapsack01.py
+++ File/SolvingKnapsackByGA/SolveKnapsack01.py
@@ -9,8 +9,67 @@ import numpy
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import knapsack
+# import knapsack
+import numpy as np
 
+class Knapsack01Problem:
+    """This class encapsulates the Knapsack 0-1 Problem from RosettaCode.org
+    """
+
+    def __init__(self,i,c):
+
+        # initialize instance variables:
+        self.items = i
+        self.maxCapacity = c
+
+        # initialize the data:
+        # self.__initData()
+
+    def __len__(self):
+        """
+        :return: the total number of items defined in the problem
+        """
+        return len(self.items)
+
+    # def __initData(self,i,c):
+    #     """initializes the RosettaCode.org knapsack 0-1 problem data
+    #     """
+    #     self.items = i
+
+    #     self.maxCapacity = c
+
+    def getValue(self, zeroOneList):
+        """
+        Calculates the value of the selected items in the list, while ignoring items that will cause the accumulating weight to exceed the maximum weight
+        :param zeroOneList: a list of 0/1 values corresponding to the list of the problem's items. '1' means that item was selected.
+        :return: the calculated value
+        """
+
+        totalWeight = totalValue = 0
+
+        for i in range(len(zeroOneList)):
+            item, weight, value = self.items[i]
+            if totalWeight + weight <= self.maxCapacity:
+                totalWeight += zeroOneList[i] * weight
+                totalValue += zeroOneList[i] * value
+        return totalValue
+
+    def printItems(self, zeroOneList):
+        """
+        Prints the selected items in the list, while ignoring items that will cause the accumulating weight to exceed the maximum weight
+        :param zeroOneList: a list of 0/1 values corresponding to the list of the problem's items. '1' means that item was selected.
+        """
+        totalWeight = totalValue = 0
+
+        for i in range(len(zeroOneList)):
+            item, weight, value = self.items[i]
+            if totalWeight + weight <= self.maxCapacity:
+                if zeroOneList[i] > 0:
+                    totalWeight += weight
+                    totalValue += value
+                    # print("- Adding {}: weight = {}, value = {}, accumulated weight = {}, accumulated value = {}".format(item, weight, value, totalWeight, totalValue))
+        print("- Total weight = {}, Total value = {}".format(totalWeight, totalValue))
+        return [totalWeight, totalValue]
 
 
 
@@ -18,9 +77,9 @@ import knapsack
 def GAKnapsack(i,c):
 # problem constants:
 # create the knapsack problem instance to be used:
-    global knapsack
+    # global knapsack
     
-    knapsack = knapsack.Knapsack01Problem(i,c)
+    knapsack = Knapsack01Problem(i,c)
 
 # Genetic Algorithm constants:b
     POPULATION_SIZE = 50
